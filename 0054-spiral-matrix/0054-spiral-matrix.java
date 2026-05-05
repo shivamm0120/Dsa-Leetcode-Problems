4bc5aa8d1e8bf7aa2baa41f970ee43489607ac4d
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-05-05 17:47:28 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.25%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -4,22 +4,30 @@ class Solution {
         int n=matrix[0].length;
 
         ArrayList<Integer>al= new ArrayList<>();
-
+//need four variables 
         int top=0;
         int left=0;
         int right=n-1;
         int bottom=m-1;
 
+
         while(top<=bottom && left<=right){
             int i;
+            //left-->right
             for(i=left;i<=right;i++)al.add(matrix[top][i]);
             top++;
+
+            //top-->right
             for(i=top;i<=bottom;i++)al.add(matrix[i][right]);
             right--;
-            if(top<=bottom){
+
+            //right-->left
+            if(top<=bottom){//need to check condition so we dont get duplicates 
                 for(i=right;i>=left;i--) al.add(matrix[bottom][i]);
                 bottom--;
             }
+
+            //bottom ->top
             if(left<=right){
                 for(i=bottom;i>=top;i--) al.add(matrix[i][left]);
                 left++;
