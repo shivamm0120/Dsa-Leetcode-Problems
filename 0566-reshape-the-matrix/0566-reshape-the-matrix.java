dad64ca09b72a99d36009151262c5b5050f498d6
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-05-05 14:18:24 +0530

    Time: 0 ms (100%), Space: 47.6 MB (5.96%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -5,27 +5,37 @@ class Solution {
 
         //valid case
         if(m*n!=r*c)return mat;
+    
+    //APPROACH-1:
+        // //create 1d array
+        // int[] arr= new int [m*n];
 
-        //create 1d array
-        int[] arr= new int [m*n];
-
-        //convert 2d array into 1d using row major ordering
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                arr[i*n+j]=mat[i][j];//this formula is used to fill the elements while converting
+        // //convert 2d array into 1d using row major ordering
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         arr[i*n+j]=mat[i][j];//this formula is used to fill the elements while converting
                 
-            }
+        //     }
+        // }
+
+        // //create 2d array which will be returned
+        // int [][]ans= new int [r][c];
+
+        // //now convert that 1d array into 2d using new column ie. 'c'
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         ans[i][j]=arr[i*c+j];
+        //     }
+       // }
+
+    //APPROACH-2:direct convert given 2d array into  1d array
+
+        int [][] ans= new int [r][c];
+
+        for(int i=0;i<m*n;i++){
+            ans[i/c][i%c]=mat[i/n][i%n];
         }
 
-        //create 2d array which will be returned
-        int [][]ans= new int [r][c];
-
-        //now convert that 1d array into 2d using new column ie. 'c'
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                ans[i][j]=arr[i*c+j];
-            }
-        }
         return ans;
     }
 }
